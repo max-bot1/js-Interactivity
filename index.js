e71c96f3802dc1061b6e76568f60f390486beb45
Max Botwin <maxmarbot@gmail.com>
2021-09-29 17:48:44 -0500

    finished lab

---
+++ index.js
@@ -0,0 +1,54 @@
+document.querySelector("form").addEventListener("submit", addMovie)
+const message = document.querySelector('aside')
+
+function  addMovie(event){
+    event.preventDefault();
+let inputField = document.querySelector("input");
+
+const movie = document.createElement("li");
+
+const movieTitle = document.createElement("span");
+movieTitle.textContent = inputField.value;
+
+movieTitle.addEventListener('click', crossOffMovie)
+
+movie.appendChild(movieTitle);
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'X'
+deleteBtn.addEventListener('click', deleteMovie)
+movie.appendChild(deleteBtn)
+
+const list = document.querySelector("ul");
+list.appendChild(movie);
+
+inputField.value = ""
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+
+    message.textContent = `${event.target.parentNode.firstChild.textContent} was successfully deleted.`
+    revealMessage()
+    
+}
+
+
+
+function crossOffMovie(event){
+event.target.classList.toggle('checked')
+
+if(event.target.classList.contains('checked')){
+    message.textContent = `${event.target.textContent} watched!`
+} else{
+    message.textContent = `Oops! ${event.target.textContent} added back.`
+}
+revealMessage()
+
+}
+
+function revealMessage(){
+    message.classList.remove('hide')
+     setTimeout(() => {
+        message.classList.add('hide')
+    }, 2000);
+}
